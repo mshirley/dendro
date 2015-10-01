b97738730c3294efbdc825cae25638277d5dfc06
mshirley <mshirley@gmail.com>
2015-10-01 02:08:22 -0600

    changes and new tool to generate kafka messages

--- dendro.py
+++ dendro.py
@@ -4,51 +4,56 @@ from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 import ujson as json
 
+sc = SparkContext(appName="dendro")
+ssc = StreamingContext(sc, 120)
+zkQuorum = "localhost"
+topic = "dendro"
+kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
+
 def getSqlContextInstance(sparkContext):
     if ('sqlContextSingletonInstance' not in globals()):
         globals()['sqlContextSingletonInstance'] = SQLContext(sparkContext)
     return globals()['sqlContextSingletonInstance']
 
-sc = SparkContext(appName="dendro")
-
-#sqlContext = SQLContext(sc)
-#df = sqlContext.read.json("/home/mshirley/src/dendro/data/test.json")
-
-ssc = StreamingContext(sc, 1)
-
-zkQuorum = "localhost"
-topic = "dendro"
-kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
-lines = kvs.map(lambda x: x[1])
-
 def process(time, rdd):
+    total = 0
     print("========= %s =========" % str(time))
     try:
         sqlContext = getSqlContextInstance(rdd.context)
-        
         lineDf = sqlContext.jsonRDD(rdd)
-        #print lineDf.select(lineDf["balance"]).count()
-        print lineDf.count()
-        
-        #lineDf.write.parquet('/home/mshirley/src/dendro/data/test1_parquet')
-        
-        #print lineDf.first()
-        #lineDf.registerTempTable("lines")
-        
-        #lineDf = sqlContext.sql("select * from lines")
-        #print lineDf.show()
-        #print type(lineDf)
-
-        # Register as table
-        #wordsDataFrame.registerTempTable("words")
-
-        # Do word count on table using SQL and print it
-        #wordCountsDataFrame = sqlContext.sql("select word, count(*) as total from words group by word")
-        #wordCountsDataFrame.show()
+	ports = lineDf.groupBy("port").count()
+        for k, v in ports.collect():
+          total += v
+	  print k, v
     except:
         pass
+    print total
 
+lines = kvs.map(lambda x: x[1])
 lines.foreachRDD(process)
 
 ssc.start()
 ssc.awaitTermination()
+
+# notes
+#sqlContext = SQLContext(sc)
+#df = sqlContext.read.json("/home/mshirley/src/dendro/data/test.json")
+#gender = lineDf.select(lineDf["gender"] == "male").count()
+#print gender 
+#total = lineDf.select(lineDf["eyeColor"] == "blue").count()
+
+#lineDf.write.parquet('/home/mshirley/src/dendro/data/test1_parquet')
+
+#print lineDf.first()
+#lineDf.registerTempTable("lines")
+
+#lineDf = sqlContext.sql("select * from lines")
+#print lineDf.show()
+#print type(lineDf)
+
+# Register as table
+#wordsDataFrame.registerTempTable("words")
+
+# Do word count on table using SQL and print it
+#wordCountsDataFrame = sqlContext.sql("select word, count(*) as total from words group by word")
+#wordCountsDataFrame.show()
---
+++ genflow.py
@@ -0,0 +1,37 @@
+import random
+import ujson as json
+from kafka import SimpleProducer, KafkaClient
+
+kafka = KafkaClient('localhost:9092')
+producer = SimpleProducer(kafka, async=True)
+
+def generateMessages(value, count):
+  queue = []
+  for c in range(0, count):
+    queue.append({"port":value})
+  return queue
+
+def msgByPortDist():
+  pick = random.randint(0, 9)
+#  if pick != 0:
+  portList = {80:10000,443:450,23:60,3389:20,445:15,1433:12,6000:9,22:6,8080:3,25:2}
+  portIdx = random.randint(0,len(portList) - 1)
+  port = portList.keys()[portIdx]
+  messages = generateMessages(port, portList[port])
+#  else:
+#    messages = generateMessages(random.randint(1, 65535), 1)
+  return messages
+
+def getMessages():
+  queue = []
+  while len(queue) < 1000000:
+    tmpQueue = msgByPortDist()
+    for i in tmpQueue:
+        queue.append(i)
+  random.shuffle(queue)
+  print "queue filled and shuffled, sending to kafka"
+  return queue 
+
+while True:
+  for m in getMessages(): 
+    producer.send_messages("dendro",json.dumps(m))
